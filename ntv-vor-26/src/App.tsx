9c197c3f8a830c1652a75ed510663947b60e56ce
Hafrún <hafrunhardar@gmail.com>
2026-02-25 23:25:31 +0000

    Features: 2 inputs and a button added

--- ntv-vor-26/src/App.tsx
+++ ntv-vor-26/src/App.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  
+  const [myName, setMyName] = useState('')
+  const [email, setEmail] = useState('')
+
   return (
     <main>
       <h1>NTV Vor 26</h1>
+
+      <div>
+        <input
+          type='text'
+          value={myName}
+          onChange={(e) => setMyName(e.target.value)}
+          placeholder='Nafn'
+        />
+
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Netfang'
+        />
+        
+        <button onClick={() => alert('Sent: ' + myName + '-' + email)}>
+            Senda 
+          </button>
+      </div>
+
     </main>
   );
 
